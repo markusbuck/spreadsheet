bd59ccbfea0e6966006f78b81f597e35689b706b
Markus Buckwalter <u1343465@utah.edu>
2023-09-02 19:21:17 -0600

    t is an integer

--- Spreadsheet/FormulaEvaluator/Evaluator.cs
+++ Spreadsheet/FormulaEvaluator/Evaluator.cs
@@ -11,6 +11,39 @@ namespace FormulaEvaluator
         {
             string[] substrings = Regex.Split(exp, "(\\()|(\\))|(-)|(\\+)|(\\*)|(/)");
 
+            Stack<int> value = new Stack<int>();
+            Stack<string> operators = new Stack<string>();
+
+            foreach(String t in substrings)
+            {
+                // if t is an integer
+                if (int.TryParse(t, out int operand1)){
+                    String topOfStack = operators.Peek();
+                    if (operators.Count > 0 && topOfStack == "*" || topOfStack == "/")
+                    {
+                        if(value.Count > 0)
+                        {
+                            int operand2 = value.Pop();
+                            string sign = operators.Pop();
+                            int product = 0;
+                            if(sign == "*")
+                            {
+                                product = operand2 * operand1;
+                            }
+                            if(sign == "/")
+                            {
+                                product = operand2 / operand1;
+                            }
+                            value.Push(product);
+                            
+                        }
+                    }
+                    else
+                    {
+                        value.Push(operand1);
+                    }
+                }
+            }
             return 1;
         }
 
