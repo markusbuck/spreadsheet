2c1a2357ad4464756f41c4552294b9fe1a43a4e4
Markus Buckwalter <u1343465@utah.edu>
2023-09-19 16:02:10 -0600

    Set up ps4

---
+++ Spreadsheet/Spreadsheet/Spreadsheet.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using SpreadsheetUtilities;
+
+namespace SS;
+
+public class Spreadsheet : AbstractSpreadsheet
+{
+	public Spreadsheet()
+	{
+	}
+
+    public override object GetCellContents(string name)
+    {
+        throw new NotImplementedException();
+    }
+
+    public override IEnumerable<string> GetNamesOfAllNonemptyCells()
+    {
+        throw new NotImplementedException();
+    }
+
+    public override IList<string> SetCellContents(string name, double number)
+    {
+        throw new NotImplementedException();
+    }
+
+    public override IList<string> SetCellContents(string name, string text)
+    {
+        throw new NotImplementedException();
+    }
+
+    public override IList<string> SetCellContents(string name, Formula formula)
+    {
+        throw new NotImplementedException();
+    }
+
+    protected override IEnumerable<string> GetDirectDependents(string name)
+    {
+        throw new NotImplementedException();
+    }
+}
+
