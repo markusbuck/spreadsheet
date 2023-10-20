b577645114b8a4e733549d347c62dde8a87d73d9
KevinSotoMiranda <levinsoto5@gmail.com>
2023-10-20 12:05:29 -0600

    Kevin: Added comments for xaml file

--- Spreadsheet/SpreadsheetGUI/MauiProgram.cs
+++ Spreadsheet/SpreadsheetGUI/MauiProgram.cs
@@ -1,22 +1,5 @@
 ﻿namespace SpreadsheetGUI;
 
-//public static class MauiProgram
-//{
-//	public static MauiApp CreateMauiApp()
-//	{
-//        var builder = MauiApp.CreateBuilder();
-//		builder
-//			.UseMauiApp<App>()
-//			.ConfigureFonts(fonts =>
-//			{
-//				fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
-//				fonts.AddFont("OpenSans-Semibold.ttf", "OpenSansSemibold");
-//			});
-
-//		return builder.Build();
-//	}
-//}
-
 using CommunityToolkit.Maui;
 using CommunityToolkit.Maui.Storage;
 
